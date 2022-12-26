7368a81d50dc1f82de90f16a072d68ea4951739f
Yash Chede <yash@Yashs-MacBook-Air.local>
2022-12-26 13:27:27 +0530

    Second commit : adding a route and a controller

---
+++ controllers/home_controller.js
@@ -0,0 +1,3 @@
+module.exports.home = function(req,res){
+ return res.end('<h1>FIRST BACKEND-SERVER</h1>')
+};
--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const port = 8000;
 const app = express();
 
+// using express router
+app.use('/',require('./routes/index'));
+
 app.listen(port,function(err){
     if (err){
         console.log(`Error in running the server : ${err}`);
